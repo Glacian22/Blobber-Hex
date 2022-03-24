9e06104a1e35d9f59fd9a854a887386584b950aa
Abram Thau <abram@c02g6153md6n.lan>
2022-03-24 11:28:27 -0700

    added dnd

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
   import HexBoard from "./components/HexBoard.svelte";
+  import Queue from './components/Queue.svelte';
+  import {gameState} from './stores';
   import "./global.css";
+
+  const execute = (actions) => {
+    actions.forEach(a => {
+      switch (a.name) {
+        case "up-right":
+          
+          break;
+      }
+    })
+  }
 </script>
 
 <main>
   <HexBoard />
+  <Queue {execute}/>
 </main>
 
 <style>
--- client/src/components/Queue.svelte
+++ client/src/components/Queue.svelte
@@ -1,7 +1,39 @@
 <script lang="ts">
-  export let execute: Function;
+  export let execute;
+  import {flip} from "svelte/animate";
+    import {dndzone} from "svelte-dnd-action";
+    let items = [
+        {id: 1, name: "up-left"},
+        {id: 2, name: "up-right"},
+        {id: 3, name: "right"},
+        {id: 4, name: "down-left"}
+    ];
+    const flipDurationMs = 300;
+    function handleDndConsider(e) {
+        items = e.detail.items;
+    }
+    function handleDndFinalize(e) {
+        items = e.detail.items;
+    }
 </script>
 
-<div id='queue'>
-  
+<div id="queue">
+  <section
+    use:dndzone={{ items, flipDurationMs }}
+    on:consider={handleDndConsider}
+    on:finalize={handleDndFinalize}
+  >
+    {#each items as item (item.id)}
+      <div animate:flip={{ duration: flipDurationMs }}>{item.name}</div>
+    {/each}
+  </section>
+  <button on:click={execute(items)}>GO</button>
 </div>
+
+<style>
+  #queue {
+    border: 1px solid black;
+    height: fit-content;
+
+  }
+</style>
