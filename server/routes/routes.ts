bd55a0647b00ce685baf3dc9e6d902f13615644a
Abram Thau <glacian22@gmail.com>
2022-04-07 16:02:02 -0700

    proxy to server working

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -1,8 +1,8 @@
 const axios = require("axios");
 const router = require("express").Router();
 
-router.get("/recipes", (req: any, res: any) => {
-  res.status = 200;
+router.get("/api", (req: any, res: any) => {
+  console.log('BOOOOOOOOOOOOOOOOOOOOOO')
   res.json({msg: 'all good amigo'})
 });
 
